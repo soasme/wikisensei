3a7c4439ab19bb81a44b109542105e4766ea4c4d
soasme <soasme@gmail.com>
2017-04-16 13:14:06 +1200

    tune for build.

--- wikisensei/settings.py
+++ wikisensei/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = './wikisensei/static'
 STATIC_URL = '/static/'
 
 

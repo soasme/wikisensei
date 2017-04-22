211c34adda13c35f933e20cabe6691bd8cd445db
soasme <soasme@gmail.com>
2017-04-23 11:05:42 +1200

    migrate to zero first on resetting.

--- wikisensei/powerpack/management/commands/reset.py
+++ wikisensei/powerpack/management/commands/reset.py
@@ -4,6 +4,7 @@ import json
 from copy import deepcopy
 from glob import glob
 
+import wikisensei.settings
 from django.core import management
 from django.core.management.base import BaseCommand, CommandError
 
@@ -12,6 +13,11 @@ class Command(BaseCommand):
     help = 'Reset dev data in database.'
 
     def handle(self, *args, **options):
+        for app in wikisensei.settings.INSTALLED_APPS:
+            try:
+                management.call_command('migrate', app, 'zero')
+            except Exception as e:
+                pass
         management.call_command('migrate')
         management.call_command('flush', verbosity=1, interactive=False)
         management.call_command('add_stub')
